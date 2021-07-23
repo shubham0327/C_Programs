05deab26a6f65c217b6f07a9705bfb3948bbf1bf
Bharath N <bharathn83@gmail.com>
2021-07-23 18:55:27 +0530

    created file str_cat.c

---
+++ string_programs/str_cat.c
@@ -0,0 +1,20 @@
+/**************
+3. Write a C program to concatenate two strings.
+***************/
+
+#include<stdio.h>
+
+int main()
+{
+	char src[20]= " world!!!";
+    char dst[20]= "Hello,";
+    int i=0,j=0;
+    while(dst[j] != '\0')
+        j++;
+    for(i = 0; src[i] != '\0'; i++,j++){
+        dst[j]=src[i];
+    }
+    dst[j] = '\0';
+	printf("Concatenated string = %s\n",dst);
+	return 0;
+}
