07f0ca468fdbc64e2c5bb7c718faf60844add8ff
Bharath N <bharathn83@gmail.com>
2021-07-27 18:43:44 +0530

    created str_rev.c file

---
+++ string_programs/str_rev.c
@@ -0,0 +1,24 @@
+/**************
+8. Write a C program to find reverse of a string.
+***************/
+
+#include<stdio.h>
+
+int main()
+{
+    char str[20]= "Hello, World!!!";
+    char tmp;
+    int i=0,len=0;
+    for(len = 0; str[len] != '\0'; len++);
+
+    for(i = 0; i<len/2 && str[i] != '\0' ; i++)
+    {
+        tmp=str[i];
+        str[i]=str[len-1-i];
+        str[len-1-i]=tmp;
+    }
+
+    printf("Reversed string = %s\n",str);
+    
+    return 0;
+}
