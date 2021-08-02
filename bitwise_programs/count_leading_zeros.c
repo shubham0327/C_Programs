29f9ca905b2df8f1cd2887fb49cfcbacf61325d6
Bharath N <bharathn83@gmail.com>
2021-08-02 19:23:23 +0530

    created count_leading_zeros.c file

---
+++ bitwise_programs/count_leading_zeros.c
@@ -0,0 +1,37 @@
+/**10. Write a C program to count leading zeros in a binary number.**/
+
+#include<stdio.h>
+#define BITS sizeof(int)*8
+
+void deci_to_bin(int num){
+    int i,k;
+    printf("\nBinary of %d is= ",num);
+    for(i=BITS-1;i>=0;i--){
+        k=num>>i;
+        printf("%d",(k&1));
+        if(i%4==0){
+            printf("\t");
+        }
+    }
+    printf("\n");
+}
+
+int main(){
+    int num,i,count=0;
+    printf("Enter the number\n");
+	scanf("%d",&num);
+
+	deci_to_bin(num);
+	for(i=BITS-1;i>=0;i--){
+        if((num>>i)&1){        //check if bit is set
+            break;
+        }
+        count++;            //count leading zeros
+	}
+    printf("number of leading zeros = %d\n",count);
+
+    return 0;
+}
+
+
+
