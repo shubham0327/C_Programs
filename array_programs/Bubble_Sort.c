98fd4ed05e96d8e47317abf3b2bf1e27e48608f0
Shubham Developer <106951233+shubham0327@users.noreply.github.com>
2025-03-25 21:56:51 +0530

    Update Bubble_Sort.c

--- array_programs/Bubble_Sort.c
+++ array_programs/Bubble_Sort.c
@@ -1,24 +1,27 @@
+/*
+Bubble sort array
+*/
+
 #include <stdio.h>
 
 int main()
 {
 int arr[]={25,15,17,10,5};
+int t;
 int ele =sizeof(arr)/sizeof(arr[0]);
+  
   for(int i=0;i<ele-1;i++)
     {
       for(int j=0;j<ele-1-i;j++)
         {
-          if(arr[i]>arr[j+1])
+          if(arr[j]>arr[j+1])
           {
-            t=arr[i];
-            arr[i]=arr[j+1];
+            t=arr[j];
+            arr[j]=arr[j+1];
             arr[j+1]=t;
           }
         }
     }
-   for(int i=0;i<ele;i++)
+  for(int i=0;i<ele;i++)
   printf("%d ",arr[i]);
-    
-
-
 }
