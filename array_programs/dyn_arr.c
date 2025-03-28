cba6bec1b6f22375afdc0633b14f1aa2881228a5
Shubham Developer <106951233+shubham0327@users.noreply.github.com>
2025-03-28 12:00:26 +0530

    Create dyn_arr.c

---
+++ array_programs/dyn_arr.c
@@ -0,0 +1,43 @@
+volatile uint32_t* const pHardwareRegister = (uint32_t*)0x00020000;
+
+
+
+// Online C compiler to run C program online
+#include <stdio.h>
+void FindNonRepEle{int arr[],size)
+{
+    int freqency[10] = {0};
+    for(int j = 0;j <size;j++)
+    {
+        freqency[arr[i]]++
+    }
+    printf("Non-Repeating elements are:");
+    for(int k = 0; k<size;k++)
+    {
+        if(frequency[arr[k]] == 1)
+        {
+            printf("%d", arr[k]);
+        }
+    }
+    printf("\n");
+}
+int main() {
+int size = 0;
+int *arr;
+printf("Enter size of an array");
+scanf("%d",&size);
+
+arr =(int*)malloc(size*sizeof(int));
+if(arr ==NULL)
+{
+    printf("Memory allocation failed");
+    return -1;
+}
+printf(" array size is %d",size);
+for(int i = 0;i<size;i++)
+{
+    scanf("%d",&arr[i]);
+}
+FindNonRepEle{int arr[],size);
+return 0;
+}
