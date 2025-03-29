834a07f0b0f16300ca2cee0365ba11e2c682cd3f
Shubham Developer <106951233+shubham0327@users.noreply.github.com>
2025-03-30 00:12:52 +0530

    Update dyn_arr.c

--- array_programs/dyn_arr.c
+++ array_programs/dyn_arr.c
@@ -2,12 +2,12 @@
 //WAP allocate memory dynamically and print the non repeating number
 #include <stdio.h>
 #include <stdlib.h>
-void FindNonRepEle(int arr[],size)
+void FindNonRepEle(int arr[],int size)
 {
     int freqeuncy[10] = {0};
     for(int j = 0;j <size;j++)
     {
-        freqeuncy[arr[i]]++;
+        freqeuncy[arr[j]]++;
     }
     
     printf("Non-Repeating elements is :\n");
@@ -23,7 +23,7 @@ void FindNonRepEle(int arr[],size)
 int main() {
 int size = 0;
 int *arr;
-printf("Enter size of an array");
+printf("Enter size of an array\n");
 scanf("%d",&size);
 
 arr =(int*)malloc(size*sizeof(int));
@@ -32,7 +32,7 @@ if(arr ==NULL)
     printf("Memory allocation failed");
     return -1;
 }
-printf(" array size is %d",size);
+printf("array size is %d \n",size);
 for(int i = 0;i<size;i++)
 {
     scanf("%d",&arr[i]);
