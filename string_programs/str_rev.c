7ba85aaea952eb7f31d51d678608a0f894625465
Shubham Developer <106951233+shubham0327@users.noreply.github.com>
2025-04-02 09:12:38 +0530

    Update str_rev.c

--- string_programs/str_rev.c
+++ string_programs/str_rev.c
@@ -15,15 +15,24 @@ int main()
     return 1;
     }
     for(len = 0; str[len] != '\0'; len++);
-
-    for(i = 0; i<len/2 && str[i] != '\0' ; i++)
+    // two ways to reverse the string
+    char ptr[len];
+    for(i=0,len=len-1;len>=0;i++,len--)
+     {
+     ptr[i]=s[len];
+     }
+     ptr[i]='\0';
+     
+    printf("Reversed string = %s\n",p);
+    
+    /* for(i = 0; i<len/2 && str[i] != '\0' ; i++)
     {
         tmp=str[i];
         str[i]=str[len-1-i];
         str[len-1-i]=tmp;
     }
 
-    printf("Reversed string = %s\n",str);
+    printf("Reversed string = %s\n",str);*/
     
     return 0;
 }
